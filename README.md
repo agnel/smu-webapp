f694d62b56b8a6a6ee421b81ea7bd81e85b81ad3
agnelwaghela <agnelwaghela@gmail.com>
2015-06-29 08:16:49 -0400

    Can? method and fonts directory setup

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # smu-webapp
 Society Management Utility Webapp
+
+Depoly this Rails project and setup a Society Management Portal.
--- smu/app/controllers/application_controller.rb
+++ smu/app/controllers/application_controller.rb
@@ -1,5 +1,27 @@
 class ApplicationController < ActionController::Base
+	
+=begin
+Every Incoming Requests Process through this Controller
+
+Method Names and its Uses:
+
+can? : Checks if a User has access to a incoming request/response
+
+=end
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+	
+	def can?(condition,level)
+    if session[:user_level].blank?
+      return false
+    else
+      if session[:user_level].send(condition, level)
+        return true
+      else
+        return false
+      end
+    end
+  end
+	
 end
